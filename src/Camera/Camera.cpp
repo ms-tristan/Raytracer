372cae2590fc8bd6852e879e49641ca34adb0876
ms-tristan <tristan.masse@epitech.eu>
2025-04-30 11:50:02 +0200

    fix: trying to fix camera parsing

--- src/Camera/Camera.cpp
+++ src/Camera/Camera.cpp
@@ -155,20 +155,30 @@ Math::Vector3D Camera::calculateRotationAngles() const {
     Math::Vector3D upDir = screen.left_side.normalize();
 
     double yRotation = atan2(viewDir.X, viewDir.Z) * 180.0 / M_PI;
+
     double xRotation = -asin(viewDir.Y) * 180.0 / M_PI;
 
-    Math::Vector3D expectedUp = Math::Vector3D(Math::Coords{0.0, 1.0, 0.0});
-    Math::Vector3D rotatedUp = Rotate("y", yRotation).applyToVector(expectedUp);
-    rotatedUp = Rotate("x", xRotation).applyToVector(rotatedUp);
+    Math::Vector3D initialViewDir(Math::Coords{0.0, 0.0, -1.0});
+    Math::Vector3D initialUpDir(Math::Coords{0.0, 1.0, 0.0});
+    Math::Vector3D initialRightDir(Math::Coords{1.0, 0.0, 0.0});
 
-    double dotProduct = upDir.dot(rotatedUp);
-    double crossLength = upDir.cross(rotatedUp).length();
+    Math::Vector3D rotatedViewDir = Rotate("y", yRotation).applyToVector(initialViewDir);
+    Math::Vector3D rotatedUpDir = Rotate("y", yRotation).applyToVector(initialUpDir);
+    Math::Vector3D rotatedRightDir = Rotate("y", yRotation).applyToVector(initialRightDir);
+
+    rotatedViewDir = Rotate("x", xRotation).applyToVector(rotatedViewDir);
+    rotatedUpDir = Rotate("x", xRotation).applyToVector(rotatedUpDir);
+    rotatedRightDir = Rotate("x", xRotation).applyToVector(rotatedRightDir);
+
+    double dotProduct = upDir.dot(rotatedUpDir);
+    double crossLength = upDir.cross(rotatedUpDir).length();
     double zRotation = atan2(crossLength, dotProduct) * 180.0 / M_PI;
 
-    Math::Vector3D crossResult = upDir.cross(rotatedUp);
+    Math::Vector3D crossResult = upDir.cross(rotatedUpDir);
     if (crossResult.dot(viewDir) < 0) {
         zRotation = -zRotation;
     }
+
     return Math::Vector3D(Math::Coords{xRotation, yRotation, zRotation});
 }
 

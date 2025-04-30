e5eaadc018b3803bd71b05c87f738236869402a7
ms-tristan <tristan.masse@epitech.eu>
2025-04-30 13:28:16 +0200

    style: fixed indentation

--- src/EventsManager/SFMLEventsManager.hpp
+++ src/EventsManager/SFMLEventsManager.hpp
@@ -6,37 +6,37 @@
 */
 
 #ifndef SRC_EVENTSMANAGER_SFMLEVENTSMANAGER_HPP_
-   #define SRC_EVENTSMANAGER_SFMLEVENTSMANAGER_HPP_
+    #define SRC_EVENTSMANAGER_SFMLEVENTSMANAGER_HPP_
 
-   #include <unordered_map>
-   #include <string>
-   #include <memory>
-   #include <SFML/Graphics.hpp>
-   #include "./IEventsManager.hpp"
+    #include <unordered_map>
+    #include <string>
+    #include <memory>
+    #include <SFML/Graphics.hpp>
+    #include "./IEventsManager.hpp"
 
 namespace RayTracer {
 
 class SFMLEventsManager : public IEventsManager {
  public:
-   explicit SFMLEventsManager(std::shared_ptr<sf::RenderWindow> window);
-   ~SFMLEventsManager() override = default;
+    explicit SFMLEventsManager(std::shared_ptr<sf::RenderWindow> window);
+    ~SFMLEventsManager() override = default;
 
-   bool isKeyPressed(std::string key) const override;
-   vector2f_t getMousePos() override;
-   float getMouseWheelDelta() const override;
+    bool isKeyPressed(std::string key) const override;
+    vector2f_t getMousePos() override;
+    float getMouseWheelDelta() const override;
 
-   bool processEvents() override;
-   bool isWindowClosed() const override;
-   bool isWindowResized() const override;
-   vector2i_t getResizedDimensions() const override;
+    bool processEvents() override;
+    bool isWindowClosed() const override;
+    bool isWindowResized() const override;
+    vector2i_t getResizedDimensions() const override;
 
  private:
-   std::shared_ptr<sf::RenderWindow> _window;
-   bool _isWindowClosed;
-   bool _isWindowResized;
-   vector2i_t _resizedDimensions;
-   sf::Event _event;
-   float _mouseWheelDelta;
+    std::shared_ptr<sf::RenderWindow> _window;
+    bool _isWindowClosed;
+    bool _isWindowResized;
+    vector2i_t _resizedDimensions;
+    sf::Event _event;
+    float _mouseWheelDelta;
 
     std::unordered_map<std::string, sf::Keyboard::Key> _keyMap;
 
